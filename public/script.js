b520ef971d88170d626af91b28ad34f6414105ae
vGJAN <vcgowsaljan007@gmail.com>
2021-02-22 23:17:17 +0000

    smartl

--- public/script.js
+++ public/script.js
@@ -35,7 +35,7 @@ navigator.mediaDevices.getUserMedia({
             toast: true,
             position: 'top-end',
             showConfirmButton: false,
-            timer: 800,
+            timer: 1500,
             timerProgressBar: true
         })
         userConnect.fire({
@@ -68,7 +68,7 @@ socket.on('user-disconnected', userId => {
         toast: true,
         position: 'top-end',
         showConfirmButton: false,
-        timer: 800,
+        timer: 1500,
         timerProgressBar: true
     })
     userDisconnect.fire({
