0bbc3a8e6af31d23f1693b6b951d8035569258c3
vGJAN <vcgowsaljan007@gmail.com>
2021-02-22 17:44:37 +0000

    smartl

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const { v4: uuidv4 } = require('uuid');
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
 
 // SERVER TO SET THE TEMPLATE ENGINE AS VIEW ENGINE TO TEMPLATE THE EJS FILE AS HTML FILE FOR SENT TO USERS
 app.set('view engine', 'ejs');
 
 // SERVER TO USE THE SCRIPT.JS FILE WHICH IS IN PUBLIC FOLDER
 app.use(express.static('public'));
+
+app.use('/peerjs', peerServer);
 
 // SERVER TO GET THE UUID THAT'S THE UNIQUE ID
 app.get('/', (req, res) => {
